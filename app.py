e1521aedfaad062281aa62e5918ca3766afd7a03
imalmadhusankaphotography-creator <imalmadhusankaphotography@gmail.com>
2026-06-15 21:41:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+PAGE_ACCESS_TOKEN = "ඔයාගේ token එක මෙතන"
+
+@app.route('/webhook', methods=['GET'])
+def verify():
+    if request.args.get('hub.verify_token') == 'mytoken123':
+        return request.args.get('hub.challenge')
+    return 'Error', 403
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    for entry in data.get('entry', []):
+        for change in entry.get('changes', []):
+            if change['field'] == 'feed':
+                item = change['value']
+                if item.get('item') == 'comment':
+                    comment_id = item['comment_id']
+                    react(comment_id)
+    return 'OK', 200
+
+def react(comment_id):
+    url = f"https://graph.facebook.com/v19.0/{comment_id}/reactions"
+    params = {
+        'type': 'LOVE',
+        'access_token': PAGE_ACCESS_TOKEN
+    }
+    requests.post(url, params=params)
+
+if __name__ == '__main__':
+    app.run()
